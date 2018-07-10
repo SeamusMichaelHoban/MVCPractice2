750f4fee624ec8cefd1d7f0670a653e81ac26840
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-07-10 11:39:45 -0400

    first commit

---
+++ MVCPractice2/Models/Customers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCPractice2.Models
+{
+    public class Customers
+    {
+        [Key]
+        public int Name { get; set; }
+        public int Address { get; set; }
+        public int Telephone { get; set; }
+    }
+}
